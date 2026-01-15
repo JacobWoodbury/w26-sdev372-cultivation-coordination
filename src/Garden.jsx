82692b7677ef6c7df5764e43a79c2498527eba1f
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2026-01-14 22:37:10 -0800

    Added initial form submission solution, needs table creation

--- src/Garden.jsx
+++ src/Garden.jsx
@@ -1,19 +1,29 @@
 import {useState} from "react";
 import Plot from "./Plot";
+
 export default function Garden() {
     const [plots, setPlots] = useState([]);
+
     function createPlot(width, length) {
         const plot = <Plot width={width} length={length} />
         setPlots([...plots, plot]);
     }
+
+    function submitForm(formData) {
+        const length = formData.get("length")
+        const width = formData.get("width")
+        createPlot(width, length)
+        console.log(plots)
+    }
+
     return (
         <div>
             <h1>Garden</h1>
-            <form>
-                <input type="text" placeholder="Plot Name" />
-                <input type="text" placeholder="Plot Description" />
-                <input type="number" placeholder="Length (ft)" />
-                <input type="number" placeholder="Width (ft)" />
+            <form action={submitForm}>
+                <input id="name" name="name" type="text" placeholder="Plot Name" />
+                <input id="description" name="description" type="text" placeholder="Plot Description" />
+                <input id="length" name="length" type="number" placeholder="Length (ft)" />
+                <input id="width" name="width" type="number" placeholder="Width (ft)" />
                 <button type="submit">Add Plot</button>
             </form>
             {plots.length > 0 && (
