fd6dd8d2e54675d025acafa6d207c121b484c9c0
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2026-01-20 14:29:56 -0800

    Commented out getPlots and savePlots to avoid usage as of right now

--- server/routes/plantsRouter.js
+++ server/routes/plantsRouter.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import {getPlants} from '../controllers/getPlants.js'
-import {getPlots} from '../controllers/getPlots.js'
 
 const router = express.Router()
 
 router.get('/api/plants', getPlants)
-router.get('/api/plots', getPlots)
 
 export default router
