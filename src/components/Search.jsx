48f76cc281f5b9bebf76b7ef16c13ace37aec0e6
Jacob Woodbury <jacobswoodbury@gmail.com>
2026-02-19 14:21:48 -0800

    adding serach component

---
+++ src/components/Search.jsx
@@ -0,0 +1,8 @@
+export default function Search() {
+    return (
+        <div>
+            <input type="text" placeholder="Search for a plant" />
+            <button>Search</button>
+        </div>
+    )
+}
